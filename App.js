7a98dc8f47e80358aa8f31e85b73d92e475978fa
Leonardo Aoki <leonardoyaoki@gmail.com>
2022-03-22 11:03:25 -0300

    primeiro commit

--- App.js
+++ App.js
@@ -1,21 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity } from 'react-native';
+import { Camera } from 'expo-camera';
+import { CameraType } from 'expo-camera/build/Camera.types';
+
+import { FontAwesome } from "@expo/vector-icons";
 
 export default function App() {
+
+  const [type, setType] = useState(Camera.Constants.Type.back)
+  const [ hasPermission, setHasPermission ] = useState(null)
+
+  useEffect (() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync()
+      setHasPermission(status === "granted");
+    }) ();
+  }, [])
+
+  if(hasPermission === null)
+    return <View/>
+  
+  if(hasPermission === false)
+    return <Text>Acesso negado</Text>
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Camera
+      style={styles.camera}
+      type={type}
+      >
+        <View style={styles.contentButtons}>
+          <TouchableOpacity
+            style={styles.buttonFlip}
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                ? Camera.Constants.Type.front
+                : Camera.Constants.Type.back
+              )
+            }}
+          >
+            <FontAwesome name="exchange" size={23} color="red"></FontAwesome>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+  },
+  camera: {
+    width: "100%",
+    height: "100%",
+  },
+  contentButtons: {
+    flex: 1,
+    backgroundColor: "transparent",
+    flexDirection: "row",
+  },
+  buttonFlip: {
+    position: "absolute",
+    bottom: 50,
+    left: 30,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#FFF",
+    margin: 20,
+    height: 50,
+    width: 50,
+    borderRadius: 50,
   },
 });
